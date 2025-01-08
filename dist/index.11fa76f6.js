a473b20d3074806cf5c4192f37f742e479f49eba
ind0zer <165696434+ind0zer@users.noreply.github.com>
2025-01-08 13:35:07 +0200

    1

---
+++ dist/index.11fa76f6.js
@@ -0,0 +1,2 @@
+let e=Handlebars.compile("<h1>Hi, I'm {{name}}. I am from {{location}}</h1>")({name:"indozer",location:"Ukraine"});document.getElementById("output").innerHTML=e;
+//# sourceMappingURL=index.11fa76f6.js.map
---
+++ dist/index.a62a5364.js
@@ -0,0 +1,2 @@
+let e=Handlebars.compile("<h1>Hi, I'm {{name}}. I am from {{location}}</h1>")({name:"indozer",location:"Ukraine"});document.getElementById("output").innerHTML=e;
+//# sourceMappingURL=index.a62a5364.js.map
