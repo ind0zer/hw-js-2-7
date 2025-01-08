44543f4ca73fe6799803843ec447c42cd10d3be4
ind0zer <165696434+ind0zer@users.noreply.github.com>
2025-01-08 13:36:47 +0200

    Updates

---
+++ index.4dc2cd6b.js
@@ -0,0 +1,2 @@
+let e=Handlebars.compile("<h1>Hi, im {{name}}. I am from {{location}}</h1>")([{name:"indozer",location:"Ukraine"}]);document.getElementById("output").innerHTML=e;
+//# sourceMappingURL=index.4dc2cd6b.js.map
